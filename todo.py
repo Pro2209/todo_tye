3136035a08bc40465605c5ec3aa7727b0449795c
TAB GEYMER <>
2021-04-22 16:06:47 +0000

    Добавили "скелет" логики бота

---
+++ todo.py
@@ -0,0 +1,32 @@
+##импорт библеотека
+
+
+
+TOCEN = ""
+
+HELP = """
+help - Вывод списка команд
+add - добавление задачи
+show - показать все задачи
+done - задача выполнена
+"""
+todo = {}
+
+print("Привет! Введите команду help для вывода списка команд")
+
+while True:
+  userAnswer = input("Введите команду\n")
+
+if userAnswer == "add" :
+  print("Работает")
+elif userAnswer == "help" :
+  print("Работает")
+elif userAnswer == "show" :
+ print("Работает")
+elif userAnswer == "done" :
+  print("Работает") 
+elif userAnswer == "exit" :
+  print("Работает")   
+else:
+    print("Error,Нет такой команды")
+    print("Введите help для вывода списка команд")
