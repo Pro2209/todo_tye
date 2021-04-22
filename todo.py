f4fe0f88e83f25a930aae042b6c20a0caf09db4a
TAB GEYMER <>
2021-04-22 16:19:59 +0000

    Реолизовали команду show

--- todo.py
+++ todo.py
@@ -15,14 +15,19 @@ todo = {}
 print("Привет! Введите команду help для вывода списка команд")
 
 while True:
-  userAnswer = input("Введите команду\n")
-
+  userAnswer = input("Введите команду:\n")
 if userAnswer == "add" :
+  userDate = input("Введите дату:\n")
+  userTask = input("Что нужно сделать?")
+
+  todo[ userDate ] = userTask
+  print(f" На{userDate} число добавлена задача '{userTask}'")
   print("Работает")
 elif userAnswer == "help" :
-  print("Работает")
+  print(HELP)
 elif userAnswer == "show" :
- print("Работает")
+ for date in todo.keys():
+   print( f"[ {date} - {todo[date]} ]")
 elif userAnswer == "done" :
   print("Работает") 
 elif userAnswer == "exit" :
